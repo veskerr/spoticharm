7b4852fb9a82c536da437a3e2e44520e81141c0f
veskerr <maximenko.bogdan@icloud.com>
2025-05-04 12:25:17 +0300

    Add files via upload

--- Spoticharm/main.py
+++ Spoticharm/main.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog
 import os
+
 from player import Ui_MainWindow as PlayerUI
 
 
@@ -33,6 +34,7 @@ class LoginWindow(QtWidgets.QMainWindow):
         self.pushButton.setGeometry(QtCore.QRect(350, 300, 150, 50))
         self.pushButton.setStyleSheet("background-color: #563C5C; color: white; font-size: 24px;")
         self.pushButton.clicked.connect(self.login)
+        folder_path = ''
 
     def login(self):
         login_text = self.lineEdit.text()
@@ -47,11 +49,30 @@ class LoginWindow(QtWidgets.QMainWindow):
         self.player_window = QtWidgets.QMainWindow()
         self.player_ui = PlayerUI()
         self.player_ui.setupUi(self.player_window)
+        self.player_ui.pushButton_6.clicked.connect(self.showFilenamesList)
         self.player_window.show()
         self.close()
 
-    def select_folder(self):
-        folder_path = QFileDialog.getExistingDirectory(self, "Вибрати папку")
+
+
+    def filter(self, files, extensions):
+        result = []
+        for filename in files:
+            for ext in extensions:
+                if filename.endswith(ext):
+                    result.append(filename)
+        return result
+
+    def showFilenamesList(self):
+        options = QFileDialog.Options()
+        file_filter = "MP3 files (*.mp3);;All files (*)"
+        file_path, _ = QFileDialog.getOpenFileName(self, "Вибрати MP3 файл", "", file_filter, options=options)
+
+        if file_path:
+            filename = os.path.basename(file_path)
+            self.player_ui.listWidget.clear()
+            self.player_ui.listWidget.addItem(filename)
+            print(filename)
 
 
 if __name__ == "__main__":
